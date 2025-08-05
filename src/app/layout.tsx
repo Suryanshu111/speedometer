fdc7cb904fc6592aa86b3a064186b63801918112
Suryanshu111 <surya9760926545@gmail.com>
2025-08-05 10:07:49 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
-        <meta name="google-site-verification" content="BdzFQMYxwVuyEqKLiCoLWdf6sKtE6Op9Wgq5DerL5kE" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
